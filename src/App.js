7c3a3f08a5922beea72e978acddc6ff9226f9956
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-09-28 12:54:11 +0200

    add game component and logic in App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  
+function Board(xIsNext, squares, onPlay) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -19,8 +16,7 @@ function Board() {
     } else {
       nextSquares[i] = "O"
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -74,10 +70,19 @@ function calculateWinner(squares) {
 }
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+  
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
@@ -85,6 +90,3 @@ export default function Game() {
     </div>
   );
 }
-
-
-//Continue with lifting state up.
